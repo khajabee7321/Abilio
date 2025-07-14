a775f41f1ac503fa00d755a83c19b71b5e77b391
srikanth <srikanth.n@abilioit.com>
2025-07-14 19:08:40 +0530

    Testing

--- src/test/java/selenium/com/First.java
+++ src/test/java/selenium/com/First.java
@@ -2,20 +2,17 @@ package selenium.com;
 
 import java.net.URL;
 import java.time.Duration;
+import com.google.common.collect.ImmutableMap;
 
 import org.openqa.selenium.By;
-import org.testng.Assert;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
-import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 public class First {
 	@Test
 	public void test() throws Exception {
-		
-	
-	
 	UiAutomator2Options options = new UiAutomator2Options();
     options.setPlatformName("Android");
    // options.setPlatformVersion("9.0");
@@ -34,9 +31,31 @@ public class First {
     }
     driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     //driver.findElement(AppiumBy.accessibilityId("I'm New, Register Now")).click();
-    driver.findElement(By.xpath("//android.widget.Button[@content-desc=\"I’m New, Register Now\"]")).click();
+    //driver.findElement(By.xpath("//android.widget.Button[@content-desc=\"I’m New, Register Now\"]")).click();
+    driver.findElement(By.xpath("//android.widget.Button[@content-desc='I’m New, Register Now']")).click();
     Thread.sleep(3000);
-    driver.findElement(By.xpath("//android.widget.EditText[@class='android.widget.EditText']"));
+  // driver.findElement(By.xpath("//android.widget.EditText[@package='com.sevaki.user']")).sendKeys("dguegf");
+  //  driver.findElement(By.xpath("//android.widget.EditText[@index='3']")).sendKeys("dguegf");
+    //System.out.println("Name is entered");
+    WebElement fullNameField = driver.findElement(By.xpath("//android.widget.EditText[@index='3']"));
+
+    if (fullNameField.isDisplayed() && fullNameField.isEnabled()) {
+        fullNameField.click(); // Focus the field
+        fullNameField.clear(); // Optional: clear any pre-filled value
+        fullNameField.sendKeys("John Doe");
+        System.out.println("Name is entered");
+    } else {
+        System.out.println("Field is not ready for input.");
+    }
+    driver.findElement(By.xpath("//android.widget.EditText[@index='4']")).click();
+    driver.findElement(By.xpath("//android.widget.EditText[@index='4']")).sendKeys("8989878987");
+    driver.findElement(By.xpath("//android.widget.EditText[@index='5']")).click();
+    driver.findElement(By.xpath("//android.widget.EditText[@index='5']")).sendKeys("test3@gmail.com");
+    driver.findElement(By.xpath("//android.widget.CheckBox[@index='7']")).click();
+    driver.findElement(By.xpath("//android.widget.Button[@content-desc='Register Now']")).click();
+    
+    
+    
 	 }
 	
 }
